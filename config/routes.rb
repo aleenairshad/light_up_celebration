bf3349e974607b85b92050d297fed8b6158b50f3
Aleena <aleenairshad123@gmail.com>
2018-02-08 01:14:14 +0500

    User, Vendor and Customer Registration done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+	devise_for :users, path: 'users'
+	# eg. http://localhost:3000/users/sign_in
+	devise_for :admins, path: 'admins'
+	# eg. http://localhost:3000/admins/sign_in
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	namespace :api do
+  		namespace :v1 do
+  			namespace :users do
+  				resources :customers
+  				resources :registrations, only:[:create]
+  				resources :sessions
+  				resources :vendors
+  			end
+  		end
+  	end
+
+	authenticated :admin do
+		root :to => 'homes#index'
+	end
+    root 'homes#index'
 end
